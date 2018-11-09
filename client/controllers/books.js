bc637e385c3d5eba80e83173f86cdc1947ece20c
Antonio <antoniodaria@msn.com>
2018-11-09 15:16:55 +0000

    Added find book details by id functionality

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -6,7 +6,14 @@ myApp.controller('BooksController', ['$scope', '$http', '$location', '$routePara
 
 	$scope.getBooks = function(){
 		$http.get('/api/books').then(function(response){
-			$scope.books = response.data; //controller/books.js : 13
+			$scope.books = response.data; 
+		});
+	}
+
+	$scope.getBook = function(){
+		var id = $routeParams.id;
+		$http.get('/api/books/'+id).then(function(response){
+			$scope.book = response.data;
 		});
 	}
 }]);
